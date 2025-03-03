168378bc7fcd931860a17e12c8659c4df135bbfe
Pwuru <mihail_popov2005@mail.ru>
2025-03-03 12:06:02 +0800

    Добавьте файлы проекта.

---
+++ tech_lab1_task2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace tech_lab1_task2
+{
+
+    public class Logic
+    { 
+         public static double[] Search(List<double> numbers)
+        {
+            if (numbers.Count < 4)
+            {
+                return null;
+            }
+
+            double[] result = new double[4];
+            List<double> sortedNumbers = numbers.OrderBy(n => n).ToList();
+
+            result[0] = sortedNumbers[sortedNumbers.Count - 1]; 
+            result[1] = sortedNumbers[sortedNumbers.Count - 2]; 
+            result[2] = sortedNumbers[0]; 
+            result[3] = sortedNumbers[1]; 
+
+            return result;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumber;
+            var numbers = new List<double>();
+
+            while (true)
+            {
+                Console.WriteLine("Введите число либо нажмите Enter, чтобы завершить ввод чисел: ");
+                inputNumber = Console.ReadLine();
+                if (string.IsNullOrEmpty(inputNumber))
+                {
+                    break;
+                }
+                numbers.Add(double.Parse(inputNumber));
+            }
+
+            double[] result = Logic.Search(numbers);
+
+            if (result == null)
+            {
+                Console.WriteLine("Необходимо ввести не менее четырех чисел.");
+            }
+            else
+            {
+                Console.WriteLine("Наибольшее число: " + result[0]);
+                Console.WriteLine("Второе наибольшее число: " + result[1]);
+                Console.WriteLine("Наименьшее число: " + result[2]);
+                Console.WriteLine("Второе наименьшее число: " + result[3]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tech_lab1_task2Tests/LogicTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tech_lab1_task2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tech_lab1_task2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void NumberOfNumbers4OrMoreTest()
+        {
+            var numbers = new List<double>() { 12, 23, 34, -56, -3438, 0, 3181, 934, -93 };
+            double[] testResult = [3181, 934, -3438, -93];
+
+            var result = Logic.Search(numbers);
+
+            Assert.AreEqual(testResult[0], result[0]);
+            Assert.AreEqual(testResult[1], result[1]);
+            Assert.AreEqual(testResult[2], result[2]);
+            Assert.AreEqual(testResult[3], result[3]);
+        }
+        
+        [TestMethod()]
+        public void NumberOfNumbersLessThan4Test()
+        {
+            var numbers = new List<double>() { 12, 23, -56 };
+            double[] testResult = [3181, 934, -3438, -93];
+
+            var result = Logic.Search(numbers);
+
+            Assert.AreEqual(null, result);
+        }
+    }
+}
